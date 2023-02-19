4f846d9110926d390289e6198168c2f54456c83d
Lucas Matheus <steve.jkl5@gmail.com>
2023-02-19 10:28:24 -0300

    adcionando autencicao

---
+++ resources/views/login/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layout.site')
+
+@section('titulo','Cursos')
+
+@section('conteudo')
+  <div class="container">
+    <h3 class="center">Entrar</h3>
+    <div class="row">
+      <form class="" action="{{route('site.login.entrar')}}" method="post">
+        {{ csrf_field() }}
+
+        <div class="input-field">
+        <label>E-mail</label>
+          <input type="text" name="email">
+        </div>
+        <div class="input-field">
+        <label>Senha</label>
+          <input type="password" name="senha">
+        </div>
+
+        <button class="btn deep-orange">Entrar</button>
+      </form>
+    </div>
+  </div>
+
+
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-//Route::get('/admin/cursos',['as'=>'admin.cursos','uses'=>'Admin\CursoController@index']);
 Route::get('/',['as'=>'site.home','uses'=>'App\Http\Controllers\Site\HomeController@index']);
-Route::get('/login',['as'=>'site.login','uses'=>'App\Http\Controllers\Site\LoginController@Index']);
+
+Route::get('/login',['as'=>'site.login','uses'=>'App\Http\Controllers\Site\LoginController@index']);
+Route::get('/login/sair',['as'=>'site.login.sair','uses'=>'App\Http\Controllers\Site\LoginController@sair']);
 Route::post('/login/entrar',['as'=>'site.login.entrar','uses'=>'App\Http\Controllers\Site\LoginController@entrar']);
-Route::get('/admin/cursos',['as'=>'admin.cursos','uses'=>'App\Http\Controllers\Admin\CursoController@index']);
-Route::get('/admin/cursos/adicionar',['as'=>'admin.cursos.adicionar','uses'=>'App\Http\Controllers\Admin\CursoController@adicionar']);
-Route::post('/admin/cursos/salvar',['as'=>'admin.cursos.salvar','uses'=>'App\Http\Controllers\Admin\CursoController@salvar']);
-Route::get('/admin/cursos/editar/{id}',['as'=>'admin.cursos.editar','uses'=>'App\Http\Controllers\Admin\CursoController@editar']);
-Route::put('/admin/cursos/atualizar/{id}',['as'=>'admin.cursos.atualizar','uses'=>'App\Http\Controllers\Admin\CursoController@atualizar']);
-Route::get('/admin/cursos/deletar/{id}',['as'=>'admin.cursos.deletar','uses'=>'Admin\CursoController@deletar']);
+
+//Route::get('/admin/cursos',['as'=>'admin.cursos','uses'=>'Admin\CursoController@index']);
+
+Route::group(['middleware'=>'auth'],function(){
+    Route::get('/admin/cursos',['as'=>'admin.cursos','uses'=>'App\Http\Controllers\Admin\CursoController@index']);
+    Route::get('/admin/cursos/adicionar',['as'=>'admin.cursos.adicionar','uses'=>'App\Http\Controllers\Admin\CursoController@adicionar']);
+    Route::post('/admin/cursos/salvar',['as'=>'admin.cursos.salvar','uses'=>'App\Http\Controllers\Admin\CursoController@salvar']);
+    Route::get('/admin/cursos/editar/{id}',['as'=>'admin.cursos.editar','uses'=>'App\Http\Controllers\Admin\CursoController@editar']);
+    Route::put('/admin/cursos/atualizar/{id}',['as'=>'admin.cursos.atualizar','uses'=>'App\Http\Controllers\Admin\CursoController@atualizar']);
+    Route::get('/admin/cursos/deletar/{id}',['as'=>'admin.cursos.deletar','uses'=>'App\Http\Admin\CursoController@deletar']);
+});
