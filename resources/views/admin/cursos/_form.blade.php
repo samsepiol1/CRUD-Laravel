f52925712754879a73ebcf18ef06531375186e50
Lucas Matheus <steve.jkl5@gmail.com>
2023-02-18 21:29:36 -0300

    commit first

---
+++ resources/views/admin/cursos/_form.blade.php
@@ -0,0 +1,38 @@
+<div class="input-field">
+  <label>Título</label>
+  <input type="text" name="titulo" value="{{isset($registro->titulo) ? $registro->titulo : ''}}">
+</div>
+
+<div class="input-field">
+  <label>Descrição</label>
+  <input type="text" name="descricao" value="{{isset($registro->descricao) ? $registro->descricao : ''}}">
+</div>
+
+<div class="input-field">
+  <label>Valor</label>
+  <input type="text" name="valor" value="{{isset($registro->valor) ? $registro->valor : ''}}">
+</div>
+
+<div class="file-field  input-field">
+
+  <div class="btn blue">
+    <span>Imagem</span>
+    <input type="file" name="imagem">
+  </div>
+  <div class="file-path-wrapper">
+    <input class="file-path validate" type="text">
+  </div>
+</div>
+@if(isset($registro->imagem))
+<div class="input-field">
+  <img width="150" src="{{asset($registro->imagem)}}" />
+</div>
+@endif
+
+<div class="input-field">
+  <p>
+      <input type="checkbox" id="test5" name="publicado" {{isset($registro->publicado) && $registro->publicado == 'sim' ? 'checked' : '' }} value="true" />
+      <label for="test5">Publicar?</label>
+    </p>
+    <br><br>
+</div>
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+//Route::get('/admin/cursos',['as'=>'admin.cursos','uses'=>'Admin\CursoController@index']);
+Route::get('/',['as'=>'site.home','uses'=>'App\Http\Controllers\Site\HomeController@index']);
+Route::get('/login',['as'=>'site.login','uses'=>'App\Http\Controllers\Site\LoginController@Index']);
+Route::post('/login/entrar',['as'=>'site.login.entrar','uses'=>'App\Http\Controllers\Site\LoginController@entrar']);
+Route::get('/admin/cursos',['as'=>'admin.cursos','uses'=>'App\Http\Controllers\Admin\CursoController@index']);
+Route::get('/admin/cursos/adicionar',['as'=>'admin.cursos.adicionar','uses'=>'App\Http\Controllers\Admin\CursoController@adicionar']);
+Route::post('/admin/cursos/salvar',['as'=>'admin.cursos.salvar','uses'=>'App\Http\Controllers\Admin\CursoController@salvar']);
+Route::get('/admin/cursos/editar/{id}',['as'=>'admin.cursos.editar','uses'=>'App\Http\Controllers\Admin\CursoController@editar']);
+Route::put('/admin/cursos/atualizar/{id}',['as'=>'admin.cursos.atualizar','uses'=>'App\Http\Controllers\Admin\CursoController@atualizar']);
+Route::get('/admin/cursos/deletar/{id}',['as'=>'admin.cursos.deletar','uses'=>'Admin\CursoController@deletar']);
